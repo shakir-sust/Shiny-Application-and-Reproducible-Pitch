0aec525d7b838d3667270cb62c933c1d536f7293
shakir-sust <shakir.sust@gmail.com>
2020-09-28 20:11:23 +0600

    The server.R file.

--- server.R
+++ server.R
@@ -1,36 +1,39 @@
-# Coursera - Developing Data Products- Course Project 
-
-
-# server.R file for the shiny app 
-
-
-# This app was developed to help people choose the best car for their trip,  
-
-# using mtcars dataset, from [R] 
-
-
-
-
-library(shiny) 
-library(datasets) 
-library(dplyr) 
-
-
-shinyServer(function(input, output) { 
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(rsconnect)
+# Select columns to be used in the analysis
+diam <- diamonds[,c(1:4,7)]
+# Define server logic required to draw a plot
+shinyServer(function(input, output) {
+  output$distPlot <- renderPlot({
+    # Select diamonds depending of user input
+    diam <- filter(diamonds, grepl(input$cut, cut), grepl(input$col, color), grepl(input$clar, clarity))
+    # build linear regression model
+    fit <- lm( price~carat, diam)
+    # predicts the price
+    pred <- predict(fit, newdata = data.frame(carat = input$car,
+                                              cut = input$cut,
+                                              color = input$col,
+                                              clarity = input$clar))
+    # Draw the plot using ggplot2
+    plot <- ggplot(data=diam, aes(x=carat, y = price))+
+      geom_point(aes(color = cut), alpha = 0.3)+
+      geom_smooth(method = "lm")+
+      geom_vline(xintercept = input$car, color = "red")+
+      geom_hline(yintercept = pred, color = "green")
+    plot
+  })
+  output$result <- renderText({
+    # Renders the text for the prediction below the graph
+    diam <- filter(diamonds, grepl(input$cut, cut), grepl(input$col, color), grepl(input$clar, clarity))
+    fit <- lm( price~carat, diam)
+    pred <- predict(fit, newdata = data.frame(carat = input$car,
+                                              cut = input$cut,
+                                              color = input$col,
+                                              clarity = input$clar))
+    res <- paste(round(pred, digits = 1.5),"$" )
+    res
+  })
   
-  
-  # Show the cars that correspond to the filters 
-  output$table <- renderDataTable({ 
-    disp_seq <- seq(from = input$disp[1], to = input$disp[2], by = 0.1) 
-    hp_seq <- seq(from = input$hp[1], to = input$hp[2], by = 1) 
-    data <- transmute(mtcars, Car = rownames(mtcars), MilesPerGallon = mpg,  
-                      GasolineExpenditure = input$dis/mpg*input$cost, 
-                      Cylinders = cyl, Displacement = disp, Horsepower = hp,  
-                      Transmission = am) 
-    data <- filter(data, GasolineExpenditure <= input$gas, Cylinders %in% input$cyl,  
-                   Displacement %in% disp_seq, Horsepower %in% hp_seq, Transmission %in% input$am) 
-    data <- mutate(data, Transmission = ifelse(Transmission==0, "Automatic", "Manual")) 
-    data <- arrange(data, GasolineExpenditure) 
-    data 
-  }, options = list(lengthMenu = c(5, 15, 30), pageLength = 30)) 
-}) 
+})
